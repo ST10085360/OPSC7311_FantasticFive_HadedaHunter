901ef5a99e2099af3af38218f8977fe519451b9e
Liam Erens <ST10085392@vcconnect.edu.za>
2023-11-13 11:25:20 +0200

    fixed images from firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,6 +59,7 @@ dependencies {
     implementation("com.google.firebase:firebase-database-ktx:20.2.2")
     implementation("com.google.android.gms:play-services-basement:18.2.0")
     implementation("com.google.firebase:firebase-auth-ktx:22.0.0")
+    implementation("com.github.bumptech.glide:glide:4.12.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
